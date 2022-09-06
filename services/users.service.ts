1442dcb333f671bf7df6738639c40d1f072e64e3
Anush Tadevosyan <anntade100@gmail.com>
2022-09-06 16:41:52 -0700

    commiting files before figuring out how to add bootstrap to angular project

---
+++ services/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginUserDTO } from 'dto/LoginUserDTO';
+import { Observable } from 'rxjs';
+import { ILoginUserResponse } from 'interface/IUser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  _url_get_All_Users = 'http://localhost:8080/todolist/getAllUsers';
+  _url_register_user = 'http://localhost:8080/todolist/registerNewUser';
+  _url_login_user = 'http://localhost:8080/todolist/login';
+
+  constructor(private _http: HttpClient) {}
+
+  loginUser(employee: LoginUserDTO): Observable<ILoginUserResponse> {
+
+    return this._http.post<ILoginUserResponse>(this._url_login_user, employee);
+
+  }
+}
